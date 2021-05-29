b46914775f7dbd0532f3c8eb29b5c809dd9b1419
SusmoySenGupta <susmoy.cse@gmail.com>
2021-05-29 19:17:46 +0600

    feat: add spinner to log in button

--- client/src/components/auth/Login.vue
+++ client/src/components/auth/Login.vue
@@ -160,7 +160,10 @@
 					<button
 						type="submit"
 						class="
-							block
+							flex
+							justify-center
+							items-center
+							gap-2
 							w-full
 							px-4
 							py-3
@@ -183,6 +186,20 @@
 						"
 					>
 						Log In
+						<span v-if="isLoading">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								class="h-4 w-4 animate-spin"
+								viewBox="0 0 20 20"
+								fill="currentColor"
+							>
+								<path
+									fill-rule="evenodd"
+									d="M4 2a1 1 0 011 1v2.101a7.002 7.002 0 0111.601 2.566 1 1 0 11-1.885.666A5.002 5.002 0 005.999 7H9a1 1 0 010 2H4a1 1 0 01-1-1V3a1 1 0 011-1zm.008 9.057a1 1 0 011.276.61A5.002 5.002 0 0014.001 13H11a1 1 0 110-2h5a1 1 0 011 1v5a1 1 0 11-2 0v-2.101a7.002 7.002 0 01-11.601-2.566 1 1 0 01.61-1.276z"
+									clip-rule="evenodd"
+								/>
+							</svg>
+						</span>
 					</button>
 				</form>
 				<hr class="w-full my-6 border-gray-300" />
@@ -209,15 +226,18 @@ export default {
 				email: "admin@admin.com",
 				password: "secret",
 			},
+			isLoading: false,
 		};
 	},
 	methods: {
 		login(user) {
+			this.isLoading = true;
 			Store.dispatch("login", user)
 				.then(() => {
 					Store.dispatch("getCurrentUser")
 						.then((currentUserResponse) => {
 							const ROLE_ID = currentUserResponse.data.role_id;
+							this.isLoading = false;
 							if (ROLE_ID === 1) {
 								this.$router.push({ name: "admin.home" });
 							} else if (ROLE_ID === 2) {
