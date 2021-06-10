fc00418508feaaa1771362dd922486bbc8951bdc
SusmoySenGupta <susmoy.cse@gmail.com>
2021-06-10 13:14:08 +0600

    fix: profile menu click outside and card border

--- client/src/views/dashboard/main.vue
+++ client/src/views/dashboard/main.vue
@@ -33,12 +33,12 @@
 								</button>
 							</li>
 							<!-- Profile menu -->
-							<li class="relative">
-								<button @click="isProfileMenuOpen = !isProfileMenuOpen" class=" align-middle rounded-full focus:shadow-outline-purple focus:outline-none" aria-label="Account" aria-haspopup="true">
+							<li class="relative" @click="isProfileMenuOpen = !isProfileMenuOpen" ref="profileMenu">
+								<button class="align-middle rounded-full focus:shadow-outline-purple focus:outline-none" aria-label="Account" aria-haspopup="true">
 									<img class="object-cover w-8 h-8 rounded-full" src="https://images.unsplash.com/photo-1502378735452-bc7d86632805?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&fit=max&s=aa3a807e1bbdfd4364d1f449eaa96d82" alt="User" aria-hidden="true" lazy-load="true"/>
 								</button>
 								<template v-if="isProfileMenuOpen">
-									<ul ref="profileMenu" class=" absolute right-0 w-56 p-2 mt-2 space-y-2 text-gray-600 bg-white border border-gray-100 rounded-md shadow-md dark:border-gray-700 dark:text-gray-300 dark:bg-gray-700" aria-label="submenu">
+									<ul class="absolute right-0 w-56 p-2 mt-2 space-y-2 text-gray-600 bg-white border border-gray-100 rounded-md shadow-md dark:border-gray-700 dark:text-gray-300 dark:bg-gray-700" aria-label="submenu">
                                         <router-link v-if="roleId == 1" :to="{name: 'admin.settings'}">
                                             <li class="flex mt-2">
                                                 <a class="inline-flex items-center w-full px-2 py-1 text-sm font-semibold transition-colors duration-150 rounded-md hover:bg-gray-100 hover:text-gray-800 dark:hover:bg-gray-800 dark:hover:text-gray-200">
